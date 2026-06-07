2c2c5df58fee94374fa33ce7aef0922ee500b2bb
Marco-Petronella <marco.petronella85@gmail.com>
2026-06-07 10:59:25 +0200

    create repo with some logic files

---
+++ routers/blogRouter.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+
+const posts = require('../data/posts')
+
+//index
+router.get("/",);
+
+//show
+router.get("/:id",);
+
+//update
+router.get("/:id",);
+
+//create
+router.get("/",);
+
+//destroy
+router.get("/:id",);
+
+module.exports(router)
---
+++ server.js
@@ -0,0 +1,13 @@
+
+const express = require('express')
+const port = 3000
+const app = express()
+const blogRouter = require('./routers/blogRouter')
+
+app.listen(port, () => {
+  console.log('Server started on port ' + port);
+})
+
+app.get('/', (req,res) => {
+  res.send('Il server funziona!')
+})
